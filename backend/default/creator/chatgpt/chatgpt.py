938d2740e0067b85e272395e8f1c82762a250bec
Yan Chen <yche4593@uni.sydney.edu.au>
2023-10-19 19:53:00 +1100

    optimize some logic

--- backend/default/creator/chatgpt/chatgpt.py
+++ backend/default/creator/chatgpt/chatgpt.py
@@ -1,8 +1,7 @@
 # Write down all the function here.
-import os
 import openai
 
-global_model = "gpt-3.5-turbo"
+global_model = "gpt-3.5-turbo-0613"
 current_model_list = {}
 
 
@@ -24,12 +23,18 @@ def get_model():
 
 
 def create_api_model(role, msg):
-    current_model_list[msg](
-        openai.ChatCompletion.create(
-            model=get_model(),
-            messages=[{"role": role, "content": msg}]
-        )
+    openai.api_key = get_api_key()
+    current_model_list[msg] = openai.ChatCompletion.create(
+        model=get_model(),
+        messages=[{"role": role, "content": msg}]
     )
+
+    # todo: add created ChatCompletion to database.
+
+
+def delete_api_model(msg):
+    # todo:delete ChatCompletion from database.
+    del current_model_list[msg]
 
 
 def get_api_model(msg):
--- backend/default/tests.py
+++ backend/default/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
-
 # Create your tests here.
+from creator.chatgpt import chatgpt
+
+chatgpt.create_api_model("user", "帮我翻译成法语")
+print(chatgpt.get_response("帮我翻译成法语"))
