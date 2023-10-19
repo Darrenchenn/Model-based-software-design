fdb0946528792682130e63aa14b86678edd28d9d
whuangbh <whuangbh@connect.ust.hk>
2023-10-19 14:56:11 +1100

    add route for login page, index page and home page, build login model

---
+++ FrontEnd/an-intelligent-image-and-text-creation-system/src/views/indexView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  router.replace({ path: '/login' })
+})
+</script>
+
+<template>
+  <div>Placeholder for index view</div>
+</template>
+
+<style scoped></style>
