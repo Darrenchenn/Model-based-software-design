d25a493cf9a1989fdec141bc818a72fe495962d6
whuangbh <whuangbh@connect.ust.hk>
2023-10-23 00:01:27 +1100

    add simple account page, history page add audit result

--- FrontEnd/an-intelligent-image-and-text-creation-system/src/views/accountView.vue
+++ FrontEnd/an-intelligent-image-and-text-creation-system/src/views/accountView.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const username = localStorage.getItem('userName')
+const identity = localStorage.getItem('identity')
+
+const onClickSignOutBtn = () => {
+  localStorage.removeItem('userName')
+  localStorage.removeItem('identity')
+  router.push('/')
+}
+</script>
 
 <template>
-  <div>Placeholder for account view</div>
+  <div class="container-fluid text-center pt-4">
+    <div class="mb-3">username: {{ username }}</div>
+    <div class="mb-3">identity: {{ identity }}</div>
+    <button @click="onClickSignOutBtn" type="button" class="btn btn-warning mb-3">Sign out</button>
+  </div>
 </template>
 
 <style scoped></style>
