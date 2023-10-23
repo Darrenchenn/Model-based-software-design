1a92b218e58d912bce94db7854eb3f757da536b1
Yan Chen <yche4593@uni.sydney.edu.au>
2023-10-23 21:34:40 +1100

    make db configurable

--- Backend/default/db/db_handle.py
+++ Backend/default/db/db_handle.py
@@ -1,14 +1,30 @@
+import configparser
+import logging
+
 from pymongo.mongo_client import MongoClient
 
+from default.common import error
+
+log = logging.getLogger('default')
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+url = config.get('mongodb', 'url')
+cluster_name = config.get('mongodb', 'cluster_name')
+log.info('url: %s--cluster_name: %s', url, cluster_name)
+print('url: %s--cluster_name: %s' % (url, cluster_name))
 # todo:these statements should be defined in config file.
-db_default_url = "mongodb+srv://c1125105680:chen9611@cluster0.zluvrrd.mongodb.net/?retryWrites=true&w=majority"
-db_default_name = "Cluster0"
+db_default_url = url
+db_default_name = cluster_name
+print('url:', db_default_url)
+print('cluster_name:', db_default_name)
 
 
 # Mongo DB handle functions starts here.
 def get_default_db_handle():
     if db_default_url == "" or db_default_name == "":
-        return error("db_name or connection_string can not be empty")
+        return error.new("db_name or connection_string can not be empty")
 
     client = MongoClient(db_default_url)
     db_handle = client[db_default_name]
@@ -17,7 +33,7 @@ def get_default_db_handle():
 
 def get_db_handle(db_name, host, port, username, password):
     if db_name == "" | host == "" | port == "" | username == "" | password == "":
-        return error("parameters can not be empty")
+        return error.new("parameters can not be empty")
 
     client = MongoClient(host=host,
                          port=int(port),
@@ -30,7 +46,7 @@ def get_db_handle(db_name, host, port, username, password):
 
 def get_db_handle_by_url(db_name, connection_string):
     if db_name == "" or connection_string == "":
-        return error("db_name or connection_string can not be empty")
+        return error.new("db_name or connection_string can not be empty")
 
     client = MongoClient(connection_string)
     db_handle = client[db_name]
